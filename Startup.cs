b554ec44c2120028ff2dbfe083d88fae09c4982c
timof <martigxnx@gmail.com>
2024-09-24 14:55:36 +0300

    Добавление проекта ASP.NET Core. Попытка настройки компонентов для создания миграций

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿using Data.Interfaces;
+using Data.Mocks;
+using Data;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using Data.Repository;
+using Microsoft.Data.SqlClient;
+using Data.Model;
+
+namespace ReviewAggregatorWebApp
+{
+    public class Startup
+    {
+        private IConfiguration _configuration;
+        private bool stringworks;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddDbContext<AppDbContext>(options => 
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddScoped<ICategoryRepository<Country>, CountryRepository>();
+            services.AddScoped<ICategoryRepository<Director>, DirectorRepository>();
+            services.AddScoped<ICategoryRepository<Genre>, GenreRepository>();
+            services.AddTransient<IAllMovies, MovieRepository>();
+            services.AddTransient<IAllUsers, UserRepository>();
+            services.AddTransient<IAllReviews, ReviewRepository>();
+        }
+
+        private static bool TestConnection(string connectionString)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка подключения: {ex.Message}");
+                return false;
+            }
+        }
+    }
+}
